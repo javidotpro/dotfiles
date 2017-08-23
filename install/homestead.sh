1fcc36e7d9908c07f76db26116e09ec3853219ef
Javier Sanchez <javier.sanchez@catalysis.co.uk>
2017-08-23 13:17:43 +0100

    Laravel Homestead added

---
+++ install/homestead.sh
@@ -0,0 +1,17 @@
+echo "Downloading laravel/homestead vagrant box, this may take some time..."
+
+vagrant box add laravel/homestead
+
+git clone https://github.com/laravel/homestead.git ~/Homestead
+
+cd ~/Homestead
+
+git checkout $HOMESTEAD_VERSION
+
+bash init.sh
+
+# Wait a bit before moving on...
+sleep 1
+
+# ...and then.
+echo "Happy Days! Homestead installed and initialized with version "$HOMESTEAD_VERSION"
