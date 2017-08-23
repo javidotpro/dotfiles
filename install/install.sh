66b6f552b38f91ab3b3a0797aabb175030af11f4
Javier Sanchez <javier.sanchez@catalysis.co.uk>
2017-08-23 14:53:21 +0100

    Install script

---
+++ install/install.sh
@@ -0,0 +1,39 @@
+echo "Select optional software to install:"
+
+echo " [1] Laravel Valet"
+echo " [2] Laravel Homestead"
+read -r -p "Install:" softwareToInstall
+
+softwareToInstall=${softwareToInstall:-3}
+
+# Install brew with packages & casks, composer and yarn global packages
+. "$DOTFILES_DIR/install/brew.sh"
+. "$DOTFILES_DIR/install/composer.sh"
+. "$DOTFILES_DIR/install/yarn.sh"
+
+if [ "$softwareToInstall" -eq 1 ] ; then
+	. "$DOTFILES_DIR/install/valet.sh"
+else 
+	if [ "$softwareToInstall" -eq 2 ] ; then
+		read -p 'Homestead version to checkout (v5.1.0): ' HOMESTEAD_VERSION
+		HOMESTEAD_VERSION=${HOMESTEAD_VERSION:-"v5.1.0"}
+
+	    . "$DOTFILES_DIR/install/homestead.sh"
+	fi
+fi
+
+# Install oh my zsh
+. "$DOTFILES_DIR/install/oh-my-zsh.sh"
+
+# Bunch of symlinks
+. "$DOTFILES_DIR/install/symlinks.sh"
+
+# Setup macos defaults and add apps to dock
+. "$DOTFILES_DIR/macos/defaults.sh"
+. "$DOTFILES_DIR/macos/dock.sh"
+
+# Clear cache
+. "$DOTFILES_DIR/bin/dotfiles" clean
+
+# Add keys from keychain to ssh agent
+ssh-add -A 2>/dev/null;
