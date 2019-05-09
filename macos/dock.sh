e1de08fd7cf5d795aaad0a3b810ced754bd2c0ec
Javier Sánchez <11959180+javidotpro@users.noreply.github.com>
2019-05-09 14:02:42 +0100

    Updated dock icons for setup

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,10 +3,10 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Skype.app"
-dockutil --no-restart --add "/Applications/App Store.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Sublime Text.app"
 dockutil --no-restart --add "/Applications/Atom.app"
